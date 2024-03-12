33d358c631be9afb571768fc7cc7119b172e4773
Marcin Pałys <marcinpatys40@gmail.com>
2024-03-12 22:18:25 +0100

    OverrideRankedGradeBooksGetLetterGradeTests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
@@ -14,26 +14,52 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             List<double> average = new List<double>();
+            int count = 0;
             for(int i=0;i<Students.Count;i++)
             {
-               average[i] = Students[i].AverageGrade;
+               average.Add(Students[i].AverageGrade);
             }
             average.Sort();
             if(Students.Count<5)
             {
                 throw new InvalidOperationException();
             }
-            int liczbaTop20Procent = (int)(average.Count * 0.2);
-            if (average.Take(liczbaTop20Procent).Contains(averageGrade))
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (averageGrade >= average[i])
+                {
+                    count++;
+                }
+            }
+            double percent = ((double)count / Students.Count);                      
+            if (percent > 0.80)
+            {
                 return 'A';
-            else if (average.Skip(liczbaTop20Procent).Take(liczbaTop20Procent).Contains(averageGrade))
+            }
+            if (percent <= 0.80 && percent >0.60)
+            {
                 return 'B';
-            else if (average.Skip(liczbaTop20Procent * 2).Take(liczbaTop20Procent).Contains(averageGrade))
+            }
+            if (percent <= 0.60 && percent > 0.40)
+            {
                 return 'C';
-            else if (average.Skip(liczbaTop20Procent * 3).Take(liczbaTop20Procent).Contains(averageGrade))
+            }
+            if (percent <= 0.40 && percent > 0.20)
+            {
                 return 'D';
-            else
-                return 'F';           
+            }           
+            return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students.");
+            }else if(Students.Count >= 5) 
+            {
+                base.CalculateStatistics();
+            }
+            
         }
     }
 }
