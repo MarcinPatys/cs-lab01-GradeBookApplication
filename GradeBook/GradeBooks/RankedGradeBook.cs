f58e604c2948223f92d8cc7d0569b6e5f77a7ca9
Marcin Pałys <marcinpatys40@gmail.com>
2024-03-12 20:41:09 +0100

    Nadpisanie metody GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            List<double> average = new List<double>();
+            for(int i=0;i<Students.Count;i++)
+            {
+               average[i] = Students[i].AverageGrade;
+            }
+            average.Sort();
+            if(Students.Count<5)
+            {
+                throw new InvalidOperationException();
+            }
+            int liczbaTop20Procent = (int)(average.Count * 0.2);
+            if (average.Take(liczbaTop20Procent).Contains(averageGrade))
+                return 'A';
+            else if (average.Skip(liczbaTop20Procent).Take(liczbaTop20Procent).Contains(averageGrade))
+                return 'B';
+            else if (average.Skip(liczbaTop20Procent * 2).Take(liczbaTop20Procent).Contains(averageGrade))
+                return 'C';
+            else if (average.Skip(liczbaTop20Procent * 3).Take(liczbaTop20Procent).Contains(averageGrade))
+                return 'D';
+            else
+                return 'F';           
         }
     }
 }
