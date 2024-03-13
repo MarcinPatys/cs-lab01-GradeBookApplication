2f4d40a41c45fdaf04e13d7eb2f9b4d13617c640
Marcin Pałys <marcinpatys40@gmail.com>
2024-03-13 14:46:31 +0100

    Modyfikacja konstruktora BaseGradeBook

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,7 +34,7 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
@@ -46,20 +46,20 @@ namespace GradeBook.UserInterfaces
                           
             if (parts[2] == "standard")
             {
-                StandardGradeBook standardGradeBook = new StandardGradeBook(name);
+                StandardGradeBook standardGradeBook = new StandardGradeBook(name, Convert.ToBoolean(parts[3]));
                 Console.WriteLine("Created gradebook {0}.", name);
                 GradeBookUserInterface.CommandLoop(standardGradeBook);
             }
             else
             if (parts[2] == "ranked")
             {
-                RankedGradeBook rankedGradeBook = new RankedGradeBook(name);
+                RankedGradeBook rankedGradeBook = new RankedGradeBook(name, Convert.ToBoolean(parts[3]));
                 Console.WriteLine("Created gradebook {0}.", name);
                 GradeBookUserInterface.CommandLoop(rankedGradeBook);
             }                      
             else
             {
-                Console.WriteLine("is not a supported type of gradebook, please try again");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
             }
         }
         
